666281871e6d327eb9e61de917bb9dd8fbdd5607
Laércio Oliveira <laercio@ar.quei.ro>
2020-12-29 18:30:54 -0300

    chore: add and config styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 62.5%;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  html,
+  body,
+  #__next {
+    height: 100%;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+export default GlobalStyles
